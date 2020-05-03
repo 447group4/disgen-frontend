0e614e4e928e000ce90e82b646a36d0c1ca15922
Troy Norton <43425563+jnorton4@users.noreply.github.com>
2020-05-02 23:28:53 -0400

    initial commit of version2

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { about } from '../data/about';
+import {Fade} from 'react-reveal';
+
+const About = () => {
+    return (
+        <div className="wrapper">
+            <div className="about">
+                {about.map((item, i) => {
+                    const {section, title, text} = item;
+                    return (
+                        <Fade key={i}>
+                            <div className="about__item">
+                                <div className="about__section">{section}</div>
+                                <div className="about__title">{title}</div>
+                                <div className="about__text">{text}</div>
+                            </div>
+                        </Fade>
+                    )
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/data/mdpage.js
@@ -0,0 +1,18 @@
+export const about = [
+    {
+        section: 'THE DATA',
+        title: 'WHERE DID IT COME FROM?',
+        text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+    },
+    {
+        section: 'THE SETUP',
+        title: 'HOW DOES THIS ALL WORK?',
+        text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+    },
+    {
+        section: 'THE TEAM',
+        title: 'THE PLAYERS BEHIND THE PROJECT',
+        text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+    },
+
+]
