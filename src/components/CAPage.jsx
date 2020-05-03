ee1c0db2d6a2fce15f081a7a520a8467a0814bca
tlinc <timlin1@umbc.edu>
2020-05-03 02:39:35 -0400

    added leaflet maps under the sections

--- src/components/CAPage.jsx
+++ src/components/CAPage.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
 import { about } from '../data/mdpage';
 import {Fade} from 'react-reveal';
+import { Map, Marker, Popup, TileLayer, Circle, CircleMarker, Polyline, Polygon, Rectangle } from "react-leaflet";
+import { Icon } from "leaflet";
 
+const center = [36.78,-119.42]
+
+const polyline = [
+  [39.29,-76.61],
+  [39.29,-76.63],
+  [39.29,-76.65],
+]
+
+const multiPolyline = [
+  [
+    [39.3, -76.6],
+    [39.3, -76.58],
+    [39.32, -76.58],
+  ],
+  [
+    [39.3, -76.65],
+    [39.3, -76.67],
+    [39.32, -76.67],
+  ],
+]
+
+const polygon = [
+  [39.35,-76.55],
+  [39.34,-76.56],
+  [39.34,-76.56],
+]
+
+const multiPolygon = [
+  [
+    [39.34,-76.55],
+    [39.34,-76.57],
+    [39.36,-76.58],
+  ],
+  [
+    [39.35,-76.52],
+    [39.355,-76.51],
+    [39.355,-76.51],
+  ],
+]
+
+const rectangle = [
+  [39.34,-76.61],
+  [39.35,-76.62],
+]
 const MDPage = () => {
     return (
         <div className="wrapper">
@@ -18,6 +64,19 @@ const MDPage = () => {
                         </Fade>
                     )
                 })}
+                <Map center={center} zoom={6.2}>
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Circle center={center} fillColor="blue" radius={200} />
+                    <CircleMarker center={[39.35, -76.6]} color="red" radius={20}>
+                    <Popup>Popup in CircleMarker</Popup>
+                    </CircleMarker>
+                    <Polygon color="purple" positions={polygon} />
+                    <Polygon color="red" positions={multiPolygon} />
+                    <Rectangle bounds={rectangle} color="black" />
+                </Map>               
             </div>
         </div>
     );
